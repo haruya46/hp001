cbe1d8dc5deea57571b4af7669de7ffc5022ad0c
pazu70 <pazu70@gmail.com>
2025-01-08 00:25:03 +0900

    no message

--- hp001/vite.config.js
+++ hp001/vite.config.js
@@ -4,7 +4,22 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/sass/app.scss',
+                'resources/sass/business.scss',
+                'resources/sass/company.scss',
+                'resources/sass/contact.scss',
+                'resources/sass/fulltime.scss',
+                'resources/sass/mobile.scss',
+                'resources/sass/parttime.scss',
+                'resources/sass/pest.scss',
+                'resources/sass/recruit.scss',
+                'resources/sass/selfemployed.scss',
+                'resources/sass/ses.scss'
+            
+            ],
             refresh: true,
         }),
     ],
