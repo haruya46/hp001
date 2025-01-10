7180311843dc47bc1557c1092594185ceea438cf
pazu70 <pazu70@gmail.com>
2025-01-10 16:21:02 +0900

    no message

--- hp001/routes/auth.php
+++ hp001/routes/auth.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-    
-    Route::post('register', [RegisteredUserController::class, 'store']);
     
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
     ->name('login');
@@ -39,7 +35,11 @@ Route::middleware('guest')->group(function () {
 
 
 Route::middleware('auth')->group(function () {
-
+    
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+    
+    Route::post('register', [RegisteredUserController::class, 'store']);
     
     Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
