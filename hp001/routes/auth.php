c3bee4a3768d49df62455cab0fdb7a89491ae585
平山晴也 <puzu70@gmail.com>
2025-04-20 22:23:55 +0900

    2025/4/20/コマンドからコミット

--- hp001/routes/auth.php
+++ hp001/routes/auth.php
@@ -30,12 +30,12 @@ Route::middleware('guest')->group(function () {
     
     Route::post('reset-password', [NewPasswordController::class, 'store'])
     ->name('password.store');
+    
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+    
+    Route::post('register', [RegisteredUserController::class, 'store']);
 });
-
-Route::get('register', [RegisteredUserController::class, 'create'])
-    ->name('register');
-
-Route::post('register', [RegisteredUserController::class, 'store']);
 
 
 
